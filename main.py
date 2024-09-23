79c1fd0bfc8326451424605c9e3f689a8e8ffbcc
Konstantin <Konstantin.Borisov@Outlook.com>
2024-09-23 21:53:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Файл главной ветки проекта до объединения")
+print("Файл главной ветки проекта после объединения")
+
+import user_input
+
+
+def main():
+  try:
+      # Запрашиваем у пользователя два аргумента
+      num1 = user_input.get_user_input("Введите первое число: ")
+      num2 = user_input.get_user_input("Введите второе число: ")
+
+      # Вычисляем и выводим результаты
+      print(f"\nРезультаты вычислений:")
+      print(f"{num1} + {num2} = {num1 + num2}")
+      print(f"{num1} - {num2} = {num1 - num2}")
+      print(f"{num1} * {num2} = {num1 * num2}")
+      print(f"{num1} / {num2} = {num1 / num2}")
+
+  except ValueError:
+      print("Ошибка: Введите корректное числовое значение.")
+main()
